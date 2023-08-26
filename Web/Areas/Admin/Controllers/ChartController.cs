f185159d482610361a995d03109229efffcbc40e
Yunus Yigit <yunusy@uia.no>
2023-08-27 00:05:47 +0200

    Fix:stripe

---
+++ Web/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Areas.Admin.Controllers;
+
+public class ChartController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
